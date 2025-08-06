497bb8950e01591cf621202f77226206328501fe
breaddo <kyla.sim.2021@smu.edu.sg>
2025-08-06 17:06:34 +0800

    frontend

--- carpark-app/src/app/app.ts
+++ carpark-app/src/app/app.ts
@@ -1,13 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Textbox } from './components/textbox/textbox';
+import { Toggle } from './components/toggle/toggle';
+import { Results } from './components/results/results';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Textbox],
+  standalone: true,
+  imports: [RouterOutlet, Textbox, Toggle, Results],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('carpark-app');
+  toggleOptions = [
+    { label: 'Night', value: 'night' },
+    { label: 'Any', value: 'any' }
+  ];
+  selectedTime = 'day';
 }
---
+++ carpark-app/src/app/components/toggle/toggle.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Input, Output, EventEmitter } from '@angular/core';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
+@Component({
+  selector: 'app-toggle',
+  imports: [MatButtonToggleModule, CommonModule],
+  templateUrl: './toggle.html',
+  styleUrl: './toggle.scss'
+})
+export class Toggle {
+  @Input() options: { label: string; value: string }[] = [];
+  @Input() selected: string = '';
+  @Output() selectedChange = new EventEmitter<string>();
+
+  onSelectionChange(value: string) {
+    this.selectedChange.emit(value);
+  }
+}
